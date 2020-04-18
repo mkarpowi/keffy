098a43f7fc4727ad8311031c0037ed7d21eab671
Michał Karpowicz <michal.karpowicz@nask.pl>
2020-04-18 19:40:30 +0200

    Added info messages

--- compile_kernel.sh
+++ compile_kernel.sh
@@ -3,9 +3,12 @@ source config.sh
 cd $KERNEL_PATH
 cp /boot/config-$(uname -r) .config
 
+echo "Installing kernel build utilities"
 sudo apt-get install build-essential libncurses-dev bison flex libssl-dev libelf-dev
 
 make menuconfig
 make -j $(nproc)
 make modules_install
+
+echo "Install kernel"
 sudo make install
