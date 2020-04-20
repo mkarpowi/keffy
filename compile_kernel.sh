c984ccdda4afe77c0c6ca4c49c994bd6fcc5c241
Michał Karpowicz <michal.karpowicz@nask.pl>
2020-04-20 12:49:45 +0200

    make local mods

--- compile_kernel.sh
+++ compile_kernel.sh
@@ -6,9 +6,10 @@ cp /boot/config-$(uname -r) .config
 echo "Installing kernel build utilities"
 sudo apt-get install build-essential libncurses-dev bison flex libssl-dev libelf-dev
 
+make localmodconfig
 make menuconfig
 make -j $(nproc)
-make modules_install
+sudo make modules_install
 
 echo "Install kernel"
 sudo make install
