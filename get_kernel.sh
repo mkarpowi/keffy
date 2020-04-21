9a5169ee091ac246fc0588f31ff010c334e34f59
Milosz Malczak <milosz.malczak@cern.ch>
2020-04-21 15:58:28 +0200

    Added error handling in get_kernel.sh

--- get_kernel.sh
+++ get_kernel.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+
 source config.sh
+f () {
+    errorCode=$? # save the exit code as the first thing done in the trap function
+    echo "ERROR: In case of using very specific version of the kernel, provide your own sources"
+    1>&2
+    exit $errorCode
+}
+trap f ERR
 
 cd $TOP_DIR
 wget https://cdn.kernel.org/pub/linux/kernel/v${LINUX_VERSION:0:1}.x/linux-$LINUX_VERSION.tar.xz
