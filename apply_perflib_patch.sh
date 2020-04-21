4a4d41b0ac506936ad684233989498ffed088ecd
Milosz Malczak <milosz.malczak@cern.ch>
2020-04-21 19:35:16 +0200

    apply_perflib_patch.sh does not replace files, but modifies them

--- apply_perflib_patch.sh
+++ apply_perflib_patch.sh
@@ -1,3 +1,94 @@
 source config.sh
-cp perflib_patches/processor_perflib-$LINUX_VERSION.c $KERNEL_PATH/drivers/acpi/processor_perflib.c
-cp perflib_patches/Makefile-$LINUX_VERSION $KERNEL_PATH/drivers/acpi/Makefile
+PERFLIB_FILE=$KERNEL_PATH/drivers/acpi/processor_perflib.c
+
+sed -i '/static DEFINE_MUTEX(performance_mutex);/a \
+\
+/*\
+ * This variable contains early init parameter acpi_cpufreq_coord_type_force\
+ * value.\
+ */\
+\
+static unsigned int coord_type_force = CPUFREQ_SHARED_TYPE_NONE;' $PERFLIB_FILE
+
+
+#sed -i '/pr->performance->shared_type = CPUFREQ_SHARED_TYPE_ALL;/d' $PERFLIB_FILE
+#sed -i '/else if (pdomain->coord_type == DOMAIN_COORD_TYPE_HW_ALL)/d' $PERFLIB_FILE
+#sed -i '/pr->performance->shared_type = CPUFREQ_SHARED_TYPE_HW;/d' $PERFLIB_FILE
+#sed -i '/else if (pdomain->coord_type == DOMAIN_COORD_TYPE_SW_ANY)/d' $PERFLIB_FILE
+#sed -i '/pr->performance->shared_type = CPUFREQ_SHARED_TYPE_ANY;/d' $PERFLIB_FILE
+
+#    bleble\
+#    blebleble/g' $PERFLIB_FILE
+
+#sed -i 's/if (pdomain->coord_type == DOMAIN_COORD_TYPE_SW_ALL)/\
+
+sed -i '/Validate the Domain info/{n;N;N;N;N;N;N;N;d}' $PERFLIB_FILE
+sed -i '/Validate the Domain info/a \
+		count_target = pdomain->num_processors;\
+\
+		\/\/ Check if coordination type forcing is requested\
+		if (coord_type_force == CPUFREQ_SHARED_TYPE_NONE) {\
+\
+			if (pdomain->coord_type == DOMAIN_COORD_TYPE_SW_ALL)\
+				pr->performance->shared_type = CPUFREQ_SHARED_TYPE_ALL;\
+			else if (pdomain->coord_type == DOMAIN_COORD_TYPE_HW_ALL)\
+				pr->performance->shared_type = CPUFREQ_SHARED_TYPE_HW;\
+			else if (pdomain->coord_type == DOMAIN_COORD_TYPE_SW_ANY)\
+				pr->performance->shared_type = CPUFREQ_SHARED_TYPE_ANY;\
+\
+		} else {\
+\
+			\/\/ Force requested coordination type\
+			pr->performance->shared_type = coord_type_force;\
+\
+			switch (coord_type_force) {\
+				case CPUFREQ_SHARED_TYPE_ALL:\
+					pr_info(PREFIX "CPU performance level coordination type forced to sw_all\\n");\
+					break;\
+				case CPUFREQ_SHARED_TYPE_ANY:\
+					pr_info(PREFIX "CPU performance level coordination type forced to sw_any\\n");\
+					break;\
+				case CPUFREQ_SHARED_TYPE_HW:\
+					pr_info(PREFIX "CPU performance level coordination type forced to hw_all\\n");\
+					break;\
+				default:\
+					pr_err(PREFIX "CPU performance level coordination type forced to unknown (%x)\\n", coord_type_force);\
+			}\
+\
+		}' $PERFLIB_FILE
+
+sed -i '/EXPORT_SYMBOL(acpi_processor_unregister_performance);/a \
+\
+\/*\
+ * Early init parameter acpi_cpufreq_coord_type_force declaration and parsing.\
+ * This parameter allows to force any of possible ACPI CPU performance\
+ * level coordination variant.\
+ *\
+ * Possible values for early init acpi_cpufreq_coord_type_force parameter:\
+ *	- sw_all\
+ *	- sw_any\
+ *	- hw_all\
+ *\/\
+\
+static int __init coord_type_force_setup(char *str)\
+{\
+\
+	if (!str)\
+		return -EINVAL;\
+\
+	pr_debug(PREFIX "Param value provided: %s", str);\
+\
+	if (strcmp(str, "sw_all") == 0)\
+		coord_type_force = CPUFREQ_SHARED_TYPE_ALL;\
+	else if (strcmp(str, "sw_any") == 0)\
+		coord_type_force = CPUFREQ_SHARED_TYPE_ANY;\
+	else if (strcmp(str, "hw_all") == 0)\
+		coord_type_force = CPUFREQ_SHARED_TYPE_HW;\
+\
+	return 0;\
+\
+}\
+\
+early_param("acpi_cpufreq_coord_type_force", coord_type_force_setup);' $PERFLIB_FILE
+
+
