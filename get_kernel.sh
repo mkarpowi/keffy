2ffd83d8499f7d1c7153d644194da14598bf0ed8
Milosz Malczak <milosz.malczak@cern.ch>
2020-04-28 10:50:12 +0200

    Added a check i kernel sources already exist

--- get_kernel.sh
+++ get_kernel.sh
@@ -9,6 +9,12 @@ f () {
 }
 trap f ERR
 
+
+if [ -d "$TOP_DIR/linux-$LINUX_VERSION" ]; then
+    echo "Kernel sources already exist"
+    exit 0
+fi
+
 cd $TOP_DIR
 wget https://cdn.kernel.org/pub/linux/kernel/v${LINUX_VERSION:0:1}.x/linux-$LINUX_VERSION.tar.xz
 tar -xvf linux-$LINUX_VERSION.tar.xz
