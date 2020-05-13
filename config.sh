918d6b88bbed3a9b3d173a9422f46d1038b7606d
Milosz Malczak <milosz.malczak@cern.ch>
2020-05-13 13:27:37 +0200

    Updated config file

--- config.sh
+++ config.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 export LINUX_VERSION=5.3.1
-#export LINUX_VERSION=4.11-rc2
 export TOP_DIR=~/work
 
 
-export CPUFREQ_REPO=git@git2.zisi.nask.pl:cpu_control/cpufreq.git
+export CPUFREQ_ADAPTIVE_REPO=git@git2.zisi.nask.pl:cpu_control/cpufreq_adaptive.git
 export TELEMETRY_REPO=git@git2.zisi.nask.pl:cpu_control/telemetry.git
-export CONTROLLER_REPO=git@git2.zisi.nask.pl:cpu_control/controller.git
 export LOAD_STRESS_REPO=git@git2.zisi.nask.pl:cpu_control/load_stress.git
 
 
 export KERNEL_PATH=$TOP_DIR/linux-$LINUX_VERSION
-export CPUFREQ_PATH=$KERNEL_PATH/drivers/cpufreq/
+export CPUFREQ_PATH=$KERNEL_PATH/drivers/cpufreq
+export CPUFREQ_ADAPTIVE_PATH=$TOP_DIR/cpufreq_adaptive
 export TELEMETRY_PATH=$TOP_DIR/telemetry
 export LOAD_STRESS_PATH=$TOP_DIR/load_stress
-export CONTROLLER_PATH=$TOP_DIR/controller
 export CONTROLLER_CONF_PATH=$PWD
