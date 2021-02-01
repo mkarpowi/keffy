38a0bb4d51bebca54224e7b2eafe4dccda87b3cc
Miłosz Malczak <milosz.malczak@nask.pl>
2021-02-01 10:05:57 +0100

    Fixed issue with kernel download for versions *.*.0

--- get_kernel.sh
+++ get_kernel.sh
@@ -16,6 +16,6 @@ if [ -d "$TOP_DIR/linux-$LINUX_VERSION" ]; then
 fi
 
 cd $TOP_DIR
-wget https://cdn.kernel.org/pub/linux/kernel/v${LINUX_VERSION:0:1}.x/linux-$LINUX_VERSION.tar.xz
-tar -xvf linux-$LINUX_VERSION.tar.xz
-rm linux-$LINUX_VERSION.tar.xz
+wget https://cdn.kernel.org/pub/linux/kernel/v${LINUX_VERSION:0:1}.x/linux-${LINUX_VERSION%.0}.tar.xz
+tar -xvf linux-${LINUX_VERSION%.0}.tar.xz && mv linux-${LINUX_VERSION%.0} linux-${LINUX_VERSION}
+rm linux-${LINUX_VERSION%.0}.tar.xz
